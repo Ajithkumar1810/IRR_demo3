7443cb18d69241247450e93c825cbab03ce87009
Ajithkumar1810 <ajithkumar.k@bigtappanalytics.net>
2022-10-20 14:49:17 +0530

    firebaseemailcode  commit

--- src/app/admin-login/admin-login.component.ts
+++ src/app/admin-login/admin-login.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
+import { initializeApp } from "firebase/app";
+import { environment } from 'src/environments/environment';
+import { getAuth, createUserWithEmailAndPassword,signInWithEmailAndPassword, signOut } from "firebase/auth";
 @Component({
   selector: 'app-admin-login',
   templateUrl: './admin-login.component.html',
@@ -14,6 +17,8 @@ export class AdminLoginComponent implements OnInit {
   Password_entered=''
   emailerror=''
   passworderror=''
+  app = initializeApp(environment.firebaseemail);
+
   constructor(private formBuilder: FormBuilder,private router: Router,) {
     this.Admin_loginform=this.formBuilder.group({
       EmailId:[''],
@@ -28,6 +33,7 @@ export class AdminLoginComponent implements OnInit {
    
     
   }
+  
   onSubmit(){
     this.Email_entered=this.Admin_loginform.value.EmailId;
     this.Password_entered=this.Admin_loginform.value.Password;
@@ -46,5 +52,57 @@ export class AdminLoginComponent implements OnInit {
       }
     
     }
+    onclickemail(){
+      this.Email_entered=this.Admin_loginform.value.EmailId;
+    this.Password_entered=this.Admin_loginform.value.Password;
+      const auth = getAuth(this.app);
+      console.log("emial:"+this.Email_entered+"Password:"+this.Password_entered)
+createUserWithEmailAndPassword(auth, this.Email_entered, this.Password_entered)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    console.log("details:"+user)
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log("error code:"+error.code)
+    console.log("error msg:"+error.message)
 
+
+    // ..
+  });
+    }
+
+    onvalidateemail(){
+      this.Email_entered=this.Admin_loginform.value.EmailId;
+    this.Password_entered=this.Admin_loginform.value.Password;
+      const auth = getAuth(this.app);
+signInWithEmailAndPassword(auth, this.Email_entered, this.Password_entered)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    console.log("signed in as:"+user)
+
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log("signin error");
+  });
+    }
+
+    onoutemail(){
+
+      const auth = getAuth(this.app);
+signOut(auth).then(() => {
+  // Sign-out successful.
+  console.log("Sign-out successful")
+}).catch((error) => {
+  // An error happened.
+  console.log("error signout")
+});
+    }
 }
--- src/app/volunteer-signup/volunteer-signup.component.ts
+++ src/app/volunteer-signup/volunteer-signup.component.ts
@@ -13,18 +13,22 @@ export class VolunteerSignupComponent implements OnInit {
   Email='Shekar@gmail.com'
   Name=''
   Phonenumber=''
+  Address=''
   Email_entered=''
   Phonenumber_entered=''
   Name_entered=''
+  Address_entered=''
   emailerror=''
   phoneerror=''
   nameerror=''
+  Addresserror=''
   successmsg=''
   constructor(private formBuilder: FormBuilder,private router: Router,) {
     this.Register_loginform=this.formBuilder.group({
       EmailId:[''],
       Name:[''],
-      PhoneNumber:['']
+      PhoneNumber:[''],
+      Address:['']
 
  
      }) 
@@ -34,6 +38,7 @@ export class VolunteerSignupComponent implements OnInit {
     this.Email_entered=this.Register_loginform.value.EmailId;
     this.Name_entered=this.Register_loginform.value.Name;
     this.Phonenumber=this.Register_loginform.value.PhoneNumber;
+    this.Address=this.Register_loginform.value.Address;
    
     
   }
@@ -41,26 +46,38 @@ export class VolunteerSignupComponent implements OnInit {
     this.Email_entered=this.Register_loginform.value.EmailId;
     this.Name_entered=this.Register_loginform.value.Name;
     this.Phonenumber=this.Register_loginform.value.PhoneNumber;
+    this.Address=this.Register_loginform.value.Address;
+
       if(this.Email!==this.Email_entered){
         this.emailerror='Invalid Email'
         this.phoneerror=''
         this.nameerror=''
+        this.Address=''
       }
       else if(this.Name!==this.Name_entered){
         this.nameerror='Invalid Name'
         this.emailerror=''
         this.phoneerror=''
+        this.Address=''
       }
       else if(this.Phonenumber!==this.Phonenumber_entered){
         this.phoneerror='Invalid Phonenumber'
         this.emailerror=''
         this.nameerror=''
+        this.Address=''
+      }
+      else if(this.Address!==this.Address_entered){
+        this.phoneerror=''
+        this.emailerror=''
+        this.nameerror=''
+        this.Address='Invalid Address'
       }
      
       else {
         this.phoneerror=''
         this.emailerror=''
         this.nameerror=''
+        this.Address=''
         this.successmsg='Registraion successfull wait for Admin to Accept!'
       }
     
